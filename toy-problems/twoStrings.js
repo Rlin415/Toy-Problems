f87c9c61597bd69cbfef9155408546e04c64b6c0
Robert Lin <Rlin415@gmail.com>
2016-02-22 11:16:29 -0800

    describe "two strings" algorithm problem

---
+++ toy-problems/twoStrings.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*
+You are given two strings, A and B. Find if there is a substring that appears in both A and B.
+
+Sample Input: twoStrings(hello, world)
+Sample Output: 'YES'
+
+Sample Input: twoStrings(hi, world)
+Sample Output: 'NO'
+
+Explanation:
+
+For the 1st test case, the letter o is common between both strings, hence the answer YES. (Furthermore, the letter l is also common, but you only need to find one common substring.)
+For the 2nd test case, hi and world do not have a common substring, hence the answer NO.
+
+*/
+
+
+function twoStrings(str1, str2) {
+  
+}
+
+module.exports = twoStrings;
